4f5646e1a48303cf0aecfc93a6613f7664092bee
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-07-16 12:56:58 -0700

    commit tree e8cfc2e0b2767eb2e2815d400117f065cb64fc7f

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 10, 'M', "black", "chill", 1),
+("Rowdy", 18, 'F', "black", "chill", 1),
+("Tabitha", 10, 'M', "black", "chill", 1),
+("Sergeant Brown", 10, 'F', "black", "chill", 1),
+("Melissa", 10, 'F', "black", "chill", 1),
+("Grinch", 10, 'M', "black", "chill", 1),
+("Wendy", 10, 'M', "black", "chill", 1),
+(NULL, 10, 'M', "black", "chill", 1);
